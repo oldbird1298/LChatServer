c2e59e296ed16b7c3821233819fef20293f95b87
dgerontop <dgerontop@OLDBIRD-PC>
2016-10-24 23:28:20 +0300

    LChatServer
    #Has added a control method to check if client is 
    	already in the list

--- src/lchatserver/LChatServer.java
+++ src/lchatserver/LChatServer.java
@@ -30,43 +30,53 @@ public class LChatServer {
         DatagramSocket sock = null;
         DatagramSocket sock_client = null;
         ArrayList<InetAddress> ips = new ArrayList<>();
+        int j = 0;
 
         try {
             // Creating a socket to receive msgs, parameter is port
             sock = new DatagramSocket(28988);
             sock_client = new DatagramSocket();
             int port_send = 27985;
-            
 
             //buffer to receive incoming messages
             byte[] buffer = new byte[65536];
             DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
             echo("Server socket created. Waiting for incoming chat messages. . .");
-            
+
             //Communication loop
             while (true) {
                 sock.receive(incoming);
                 byte[] data = incoming.getData();
-                String s = new String(data, 0 , incoming.getLength());
-                
+                String s = new String(data, 0, incoming.getLength());
+
                 //adding the client to the chat Room
-                
                 if (s.equals("welcome")) {
-                    ips.add(incoming.getAddress());
+                    j = 0;
+                    for (InetAddress dupclicate : ips) {
+                        if (incoming.getAddress().toString() == dupclicate.getAddress().toString()) {
+                            j = 1;
+                        }
+                    }
+                    if (j != 1) {
+                        ips.add(incoming.getAddress());
+                    }
+                }else if (s.equals("bye")) {
+                    if (ips.contains(incoming.getAddress())) {
+                        ips.remove(ips.indexOf(incoming.getAddress()));
+                    }   
                 }
-                
-                echo (incoming.getAddress().getHostAddress() + ":" + incoming.getAddress().getHostName() + " : " + incoming.getPort() + "->" + s  );
-                s = "OK : " + s ;
+
+                echo(incoming.getAddress().getHostAddress() + ":" + incoming.getAddress().getHostName() + " : " + incoming.getPort() + "->" + s);
+                s = "OK : " + s;
                 for (InetAddress ips1 : ips) {
-                System.out.println(ips);
-                DatagramPacket dp = new DatagramPacket(s.getBytes(), s.getBytes().length, ips1, port_send);
-                sock_client.send(dp);
-                
+                    System.out.println(ips);
+                    echo (ips1.getAddress() + ":" + ips1.getHostName() + ":" + port_send + "->" + s);
+                    DatagramPacket dp = new DatagramPacket(s.getBytes(), s.getBytes().length, ips1, port_send);
+                    sock_client.send(dp);
+
                 }
-                
-            
+
             }
-            
 
         } catch (IOException e) {
             System.out.println("IOException : " + e);
