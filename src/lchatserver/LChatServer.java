2e5d79627e5cf58a0ce6f45603fa5ecc660ddab2
dgerontop <dgerontop@OLDBIRD-PC>
2016-11-11 01:54:36 +0200

    LChat Server Version 1.14 (Clear) build (00014)
    The fresh LChat Server has critical corrections in the code
    and it is more ready for futures updates.
    LChat Server 2 is coming

--- src/lchatserver/LChatServer.java
+++ src/lchatserver/LChatServer.java
@@ -20,6 +20,8 @@ import java.net.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LChatServer {
 
@@ -53,49 +55,41 @@ public class LChatServer {
                 sock.receive(incoming);
                 byte[] data = incoming.getData();
                 String s = new String(data, 0, incoming.getLength());
+                echoDetails(incoming.getAddress().getCanonicalHostName() + ":" + incoming.getAddress().getHostName() + ":" + incoming.getPort() + "->" + s, true);
 
+                /*
+                Check the incoming String if is something of protocol
+                 */
                 //adding the client to the chat Room
+                //Also Advertise new Client to Others
                 if (s.startsWith("welcome")) {
-                    ConHosts newClient = new ConHosts();
-                    j = 0;
-                    for (ConHosts dupclicate : connected) {
-                        System.out.println(incoming.getAddress().getHostAddress() + "::" + dupclicate.getHost().getHostAddress());
-                        if (incoming.getAddress().getHostAddress() == dupclicate.getHost().getHostAddress() || incoming.getAddress().getHostAddress().equals(dupclicate.getHost().getHostAddress())) {
-                            s = dupclicate.Name + ": " + s;
-                            j = 1;
-                        }
+
+                    try {
+                        insertElementsToList(connected, incoming, s);
+                    } catch (Exception ex) {
+                        Logger.getLogger(LChatServer.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                    if (j != 1) {
-                        String[] nickname = s.split(" ");
-                        newClient.Name = nickname[1];
-                        newClient.host = incoming.getAddress();
-                        connected.add(newClient);
+                    //Removes a client from the list
+                } else if (s.startsWith("USER_02")) {
+
+                    try {
+                        removeElementsFromList(connected, incoming, s);
+                    } catch (Exception ex) {
+                        Logger.getLogger(LChatServer.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                } else if (s.equals("bye")) {
-                    if (connected.contains(incoming.getAddress())) {
-                        connected.remove(connected.indexOf(incoming.getAddress()));
-                    }
+                    //Send the list of clients to the connected pc's.
                 } else if (s.equals("GET")) {
                     for (ConHosts ips3 : connected) {
                         String hosts = ips3.Name;
-                        hosts = "USER " + hosts;
+                        hosts = "USER_01 " + hosts;
                         DatagramPacket dp = new DatagramPacket(hosts.getBytes(), hosts.getBytes().length, incoming.getAddress(), port_send);
                         sock_client.send(dp);
 
                     }
                 }
-                echo(ft.format(tNow));
-                for (int i = 0; i < 10; i++) {
-                    System.out.print("=");
-                }
-                System.out.println("");
-                echo(" " + incoming.getAddress().getHostAddress() + ":" + incoming.getAddress().getHostName() + " : " + incoming.getPort() + "->" + s);
 
-                for (int i = 0; i < 10; i++) {
-                    System.out.print("=");
-                }
                 System.out.println("");
-                //s = "OK : " + s;
+
                 for (ConHosts ips2 : connected) {
                     if (incoming.getAddress().getHostAddress() == ips2.getHost().getHostAddress() || incoming.getAddress().getHostAddress().equals(ips2.getHost().getHostAddress())) {
                         s = ips2.Name + ": " + s;
@@ -104,14 +98,9 @@ public class LChatServer {
                 }
 
                 for (ConHosts ips1 : connected) {
-                    echo(ft.format(tNow));
-                    for (int i = 0; i < 10; i++) {
-                        System.out.print("=");
-                    }
-                    System.out.println("");
-                    System.out.println(ips1.getHost().getHostAddress());
-                    echo(ft.format(tNow) + " " + ips1 + ":" + ips1.getHost().getHostName() + ":" + port_send + "->" + s);
-
+//                    System.out.println(ips1.getHost().getHostAddress());
+                    echoDetails(ips1 + ":" + ips1.getHost().getHostName() + ":" + port_send + "->" + s, false);
+                    //System.out.println("=================");
                     DatagramPacket dp = new DatagramPacket(s.getBytes(), s.getBytes().length, ips1.getHost(), port_send);
                     sock_client.send(dp);
 
@@ -128,6 +117,105 @@ public class LChatServer {
 
     public static void echo(String msg) {
         System.out.println(msg);
+    }
+
+    public static void echoDetails(String msg, boolean in) {
+        Date tNow = new Date();
+        SimpleDateFormat ft1 = new SimpleDateFormat("EEE dd MMM YYYY");
+        SimpleDateFormat ft2 = new SimpleDateFormat("hh:mm:ss");
+        String inOut = null;
+
+        if (in) {
+            inOut = "IN";
+        } else {
+            inOut = "OUT";
+        }
+        //echo(ft1.format(tNow));
+        System.out.println(ft1.format(tNow) + "\t" + inOut + "\t" + ft2.format(tNow) + "\t" + msg);
+    }
+
+    public static void insertElementsToList(ArrayList<ConHosts> e, DatagramPacket pack, String s) throws Exception {
+        int j = 0;
+        String nMessage = null;
+        ConHosts n = new ConHosts();
+
+        for (ConHosts el : e) {
+            //if (!e.contains(pack.getAddress()));
+            if (el.host.getAddress().equals(pack.getAddress())) {
+                j = 1;
+            }
+        }
+        if (j != 1) {
+            String[] nHost = s.split(" ");
+            n.Name = nHost[1];
+            n.host = pack.getAddress();
+            sendData(e, "USER_01 " + n.Name);
+            e.add(n);
+
+        }
+        System.out.printf("The List Size is : %d", e.size());
+
+    }
+
+    public static void removeElementsFromList(ArrayList<ConHosts> e, DatagramPacket pack, String s) throws Exception {
+        int j = 0;
+        String nMessage = null;
+        ConHosts n = new ConHosts();
+        String[] rm = s.split(" ");
+        for (ConHosts el : e) {
+            //if (!e.contains(pack.getAddress()));
+            n.Name = el.Name;
+            if (n.getName().equals(rm[1])) {
+                j = e.indexOf(el);
+
+            }
+        }
+        e.remove(j);
+        sendData(e, "USER_02 " + rm[1]);
+        System.out.printf("The List Size is : %d", e.size());
+    }
+
+    public boolean checkList(ArrayList<ConHosts> e, DatagramPacket in) {
+        boolean check = true;
+        for (ConHosts test : e) {
+            if (test.host.getHostAddress().equals(in.getAddress().getHostAddress())) {
+                check = true;
+            } else {
+                check = false;
+            }
+        }
+        return check;
+    }
+
+    public static void sendData(ArrayList<ConHosts> ec, String msg) {
+        int port_send = 27985;
+        Date tNow = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat("EEE dd MMM - dd-MM-yy - hh:mm:ss ");
+        try {
+
+            DatagramSocket opr = new DatagramSocket();
+
+            for (ConHosts el : ec) {
+                System.out.println("");
+                echo(ft.format(tNow) + "Sending Message TO : ->->");
+                for (int i = 0; i < 25; i++) {
+                    System.out.print("=");
+                }
+                System.out.println("");
+                System.out.println(el.host.getHostName() + ":" + el.Name + " -> " + msg);
+                System.out.println("===============================");
+                DatagramPacket inform = new DatagramPacket(msg.getBytes(), msg.length(), el.host, port_send);
+                opr.send(inform);
+
+            }
+            //opr.close();
+
+        } catch (SocketException ex) {
+            Logger.getLogger(LChatServer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(LChatServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
 }
